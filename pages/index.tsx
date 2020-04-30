f4d8407a5d96521eca0418cb23b922709ed97d05
Francesco Cappelli <francesco.cappelli.developer@gmail.com>
2020-04-30 22:36:28 +0100

    add sass support to next

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
-const Home = () => <h1>Hello world!</h1>;
+import css from './index.scss';
+
+const Home = () => (
+  <div>
+    <h1>Hello world!</h1>
+    <p className={css.paragraph}>ciaoneee</p>
+  </div>
+);
 
 export default Home;
