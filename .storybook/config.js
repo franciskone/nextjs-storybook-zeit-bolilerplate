9e9842526326ab02cdf7de6a7699fe49fd3a246e
Francesco Cappelli <francesco.cappelli.developer@gmail.com>
2020-05-03 19:02:55 +0100

    add global addons to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,9 @@
-import { configure } from '@storybook/react';
+import { addDecorator, configure } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+import { withA11y } from '@storybook/addon-a11y';
 
 // automatically import all files ending in *.stories.tsx
 configure(require.context('../src', true, /\.stories\.tsx?$/), module);
+
+addDecorator(withKnobs);
+addDecorator(withA11y);
--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-import { withKnobs, text } from "@storybook/addon-knobs";
+import { text } from "@storybook/addon-knobs";
 import { action } from "@storybook/addon-actions";
-import { withA11y } from '@storybook/addon-a11y';
 
 import Button from "./Button";
 
 export default {
   title: "Button",
-  decorators: [withKnobs, withA11y],
   component: Button,
 };
 
